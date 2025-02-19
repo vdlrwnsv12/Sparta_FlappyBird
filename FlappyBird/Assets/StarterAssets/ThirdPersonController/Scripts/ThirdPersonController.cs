54d18b50113651cfabcd0345367f0665c886218a
vdlrwnsv12 <vdlrwnsv12@gmail.com>
2025-02-19 16:35:06 +0900

    메타버스에 블록 놓기

--- FlappyBird/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ FlappyBird/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -196,7 +196,7 @@ namespace StarterAssets
             if (_input.look.sqrMagnitude >= _threshold && !LockCameraPosition)
             {
                 //Don't multiply mouse input by Time.deltaTime;
-                float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
+                float deltaTimeMultiplier = IsCurrentDeviceMouse ? 4.0f : Time.deltaTime;
 
                 _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier;
                 _cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier;
